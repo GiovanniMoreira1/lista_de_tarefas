593d82043c0ecb052afd46bb6f75ee1de3359ba5
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2024-04-09 23:29:36 +0000

    uso do define para o tamanho da descricao e da categoria

--- tarefas.c
+++ tarefas.c
@@ -13,10 +13,10 @@ ERROS criar(Tarefa tarefas[], int *pos){
   
     clearBuffer();
     printf("Entre com a categoria: ");
-    fgets(tarefas[*pos].categoria, 100, stdin);
+    fgets(tarefas[*pos].categoria, T_CATEGORIA, stdin);
     tarefas[*pos].categoria[strcspn(tarefas[*pos].categoria, "\n")] = '\0';
     printf("Entre com a descricao: ");
-    fgets(tarefas[*pos].descricao, 300, stdin);
+    fgets(tarefas[*pos].descricao, T_DESCRICAO, stdin);
     tarefas[*pos].descricao[strcspn(tarefas[*pos].descricao, "\n")] = '\0';
 
 
