b3b2836fd4bcba9c878a8219a93b65bc29ed59ff
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2024-04-09 23:23:43 +0000

    correção dos bugs - quebra de linha no final da string e tratamento dos erros na main

--- main.c
+++ main.c
@@ -20,13 +20,33 @@ int main(){
         printf("Escolha uma opcao: ");
 
         scanf("%d", &opcao);
+
         opcao--;
         if(opcao > 2)
             printf("Opcao invalida\n");
-        else if(opcao >= 0)
+        else if(opcao >= 0){
             fs[opcao](tarefas, &pos);
-        else
-            printf("Sair...\n");
+    
+            if (erro == MAX_TAREFA) {
+                printf("ERRO - Máximo de tarefas atingido.\n");
+            } else if (erro == CRIAR) {
+                printf("ERRO - Não foi possível criar o arquivo.\n");
+            } else if (erro == SEM_TAREFAS) {
+                printf("ERRO - Não há nenhuma tarefa escrita.\n");
+            } else if (erro == NAO_ENCONTRADO) {
+                printf("ERRO - Posição não encontrada.\n");
+            } else if (erro == ABRIR) {
+                printf("ERRO - Não foi possível abrir o arquivo.\n");
+            } else if (erro == FECHAR) {
+                printf("ERRO - Não foi possível fechar o arquivo.\n");
+            } else if (erro == ESCREVER) {
+                printf("ERRO - Não foi possível escrever em seu arquivo.\n");
+            } else if (erro == LER) {
+                printf("ERRO - Não foi possível ler o seu arquivo.\n");
+            }
+    }
+    else
+        printf("Sair...\n");
 
     } while(opcao >= 0);
 
--- tarefas.c
+++ tarefas.c
@@ -14,10 +14,11 @@ ERROS criar(Tarefa tarefas[], int *pos){
     clearBuffer();
     printf("Entre com a categoria: ");
     fgets(tarefas[*pos].categoria, 100, stdin);
-    clearBuffer();
+    tarefas[*pos].categoria[strcspn(tarefas[*pos].categoria, "\n")] = '\0';
     printf("Entre com a descricao: ");
     fgets(tarefas[*pos].descricao, 300, stdin);
-    clearBuffer();
+    tarefas[*pos].descricao[strcspn(tarefas[*pos].descricao, "\n")] = '\0';
+
 
     *pos = *pos + 1;
 
@@ -53,9 +54,9 @@ ERROS listar(Tarefa tarefas[], int *pos){
         return SEM_TAREFAS;
 
     for(int i=0; i<*pos; i++){
-        printf("Pos: %d\t", i+1);
-        printf("Prioridade: %d\t", tarefas[i].prioridade);
-        printf("Categoria: %s\t", tarefas[i].categoria);
+        printf("Pos: %d     ", i+1);
+        printf("Prioridade: %d     ", tarefas[i].prioridade);
+        printf("Categoria: %s     ", tarefas[i].categoria);
         printf("Descricao: %s\n", tarefas[i].descricao);
     }
 
