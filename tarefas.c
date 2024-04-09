a92b19653549bc8b3167e75b4e1cfd6edfb1c4d7
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2024-04-09 18:27:07 +0000

    Remoção da quebra de linha

--- tarefas.c
+++ tarefas.c
@@ -11,10 +11,11 @@ ERROS criar(Tarefa tarefas[], int *pos){
     clearBuffer();
     printf("Entre com a categoria: ");
     fgets(tarefas[*pos].categoria, 100, stdin);
-
+    clearBuffer();
     printf("Entre com a descricao: ");
     fgets(tarefas[*pos].descricao, 300, stdin);
-
+    clearBuffer();
+    
     *pos = *pos + 1;
 
     return OK;
