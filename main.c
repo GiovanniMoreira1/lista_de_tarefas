2b816536b720a22738c7a0c63a7c62272aac3875
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2024-04-12 17:47:12 +0000

    correção de bugs atualizada - erro aparecendo para todas as funções

--- main.c
+++ main.c
@@ -25,25 +25,27 @@ int main(){
         if(opcao > 2)
             printf("Opcao invalida\n");
         else if(opcao >= 0){
-            fs[opcao](tarefas, &pos);
+            int ERRO = fs[opcao](tarefas, &pos);
     
-            if (erro == MAX_TAREFA) {
+            if (ERRO == MAX_TAREFA) {
                 printf("ERRO - Máximo de tarefas atingido.\n");
-            } else if (erro == CRIAR) {
+            } else if (ERRO == CRIAR) {
                 printf("ERRO - Não foi possível criar o arquivo.\n");
-            } else if (erro == SEM_TAREFAS) {
+            } else if (ERRO == SEM_TAREFAS) {
                 printf("ERRO - Não há nenhuma tarefa escrita.\n");
-            } else if (erro == NAO_ENCONTRADO) {
+            } else if (ERRO == NAO_ENCONTRADO) {
                 printf("ERRO - Posição não encontrada.\n");
-            } else if (erro == ABRIR) {
+            } else if (ERRO == ABRIR) {
                 printf("ERRO - Não foi possível abrir o arquivo.\n");
-            } else if (erro == FECHAR) {
+            } else if (ERRO == FECHAR) {
                 printf("ERRO - Não foi possível fechar o arquivo.\n");
-            } else if (erro == ESCREVER) {
+            } else if (ERRO == ESCREVER) {
                 printf("ERRO - Não foi possível escrever em seu arquivo.\n");
-            } else if (erro == LER) {
+            } else if (ERRO == LER) {
                 printf("ERRO - Não foi possível ler o seu arquivo.\n");
-            }
+            } else if (ERRO == OK) {
+            } else {
+        }
     }
     else
         printf("Sair...\n");
